0114b40e49d04e30a93d89a391723882e65eca98
Uriel Cisneros <urielcisne96@gmail.com>
2025-01-14 10:48:22 -0600

    add formater currency

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 
+var currencyFormatter = require('currency-formatter');
 
-const ahorro_year = 0;
-let ahorro_now = 1000000;
-let years = 5;
-let interes = 0.09;
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< CONSTANTS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 
-for (let i = 0; i < years; i++) {
-    ahorro_now += ahorro_now * interes;
-    ahorro_now += ahorro_year;
-    console.log('Uriel',{
-      ahorro_now: ahorro_now.toFixed(2),
-      year:i +1
+const SAVINGS_YEARS = 0; //Dinero que puedes ahorrar por año
+const SAVINGS_NOW = 1000000; //Dinero que ya tienes ahorrado y con el cual iniciaras
+const YEARS_OF_SAVING = 5; //Años que estaras ahorrando
+const ANNUAL_INTEREST = 0.09; //Interes que ganaras anual
+
+
+const formatMoney = amount => currencyFormatter.format(amount, { code: 'MX' });
+
+let saving_calculate_years = SAVINGS_NOW;
+
+for (let i = 0; i < YEARS_OF_SAVING; i++) {
+    saving_calculate_years += saving_calculate_years * ANNUAL_INTEREST;
+    saving_calculate_years += SAVINGS_YEARS;
+    console.table({
+      Ahorro_por_año:  `$${formatMoney(parseFloat(saving_calculate_years.toFixed(2)))}`,
+      Año_de_ahorro :i + 1
     });
 }
 
+console.table({
+  Ahorros: `$${formatMoney(parseFloat(saving_calculate_years.toFixed(2)))}`,
+  Años: YEARS_OF_SAVING,
+  Ganancia: `$${formatMoney(parseFloat(saving_calculate_years - 4000 - (SAVINGS_YEARS * YEARS_OF_SAVING)).toFixed(2))}`
+})
 
-console.log(`Tienes $${ahorro_now.toFixed(2)} en ${years} años`);
-
-console.log(`Ganancia es de <<< $${(ahorro_now - 4000 - (ahorro_year * years)).toFixed(2)} >>> en ${years} años`);
